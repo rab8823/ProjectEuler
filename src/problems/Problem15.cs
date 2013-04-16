3c96430ed30fd7a1b7bb57b5ecf75af84fd2f535
Andrew Barefoot <rab8823@gmail.com>
2013-04-15 22:55:48 -0400

    finished problem 15

---
+++ src/problems/Problem15.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ProjectEuler
+{
+	public class Problem15
+	{
+		public Problem15 ()
+		{
+		}
+	}
+}
+
---
+++ src/utilities/Point.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ProjectEuler
+{
+	public class Point
+	{
+		public Point ()
+		{
+		}
+	}
+}
+
